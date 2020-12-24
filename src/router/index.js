85f77aff45235ad689a07b533eee7cd4364bfb8b
林泽权 <1164095457@qq.com>
2020-12-24 09:36:01 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/Layout'),
+    redirect:'/home',
+    children:[
+      { path:'/home', component: () => import('@/views/Home')},
+      { path:'/form', component: () => import('@/views/Form')},
+      { path:'/table', component: () => import('@/views/Table')},
+      { path:'/excel', component: () => import('@/views/Excel')},
+      { path:'/editor', component: () => import('@/views/Editor')},
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Editor/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <el-card class="box-card" v-loading="loading">
+            <el-breadcrumb separator="/">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/form' }">编辑器</el-breadcrumb-item>
+            </el-breadcrumb>
+            <el-divider></el-divider>
+
+
+
+
+            
+           
+        </el-card>
+
+        
+    </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        loading:true
+      }
+    },
+    created(){
+        setTimeout(()=>{
+            this.loading = false
+        },500)
+    },
+    
+  }
+</script>
